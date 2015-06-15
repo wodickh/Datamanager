88f1c95105dfda08fcebd6df54a73ba2b14ac6d9
wodickh <michael@skotte-wodickh.dk>
2015-06-15 22:31:23 +0200

    Added Item datatype

---
+++ Nota.DataManagement.Model/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nota.DataManagement.Model
+{
+    public class Item : BaseData
+    {
+        public string Title { get; set; }
+        public override void CreateTombstone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
